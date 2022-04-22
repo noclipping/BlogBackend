9afb897b0ea0a6671003a41198e968f647093eaf
aresous <aresous@gmail.com>
2022-04-21 21:54:14 -0400

    validation and error handling

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,56 +1,71 @@
-const Post = require("../models/post");
-const { body, validationResult } = require("express-validator");
+const Post = require('../models/post')
+const { body, validationResult } = require('express-validator')
 exports.post_list = function (req, res, next) {
-  Post.find({}, (err, results) => {
-    res.send(results);
-  });
-};
+    Post.find({}, (err, results) => {
+        res.send(results)
+    })
+}
 exports.create_post = [
-  body("title")
-    .trim()
-    .isLength({ min: 1 })
-    .escape()
-    .withMessage("Author must be specified."),
-  body("content")
-    .trim()
-    .isLength({ min: 1 })
-    .escape()
-    .withMessage("Content must be specified."),
-  body("author").trim().isLength({ min: 1 }).escape(),
+    body('title')
+        .trim()
+        .isLength({ min: 1 })
+        .escape()
+        .withMessage('Author must be specified.'),
+    body('content')
+        .trim()
+        .isLength({ min: 1 })
+        .escape()
+        .withMessage('Content must be specified.'),
+    body('author').trim().isLength({ min: 1 }).escape(),
 
-  async function (req, res, next) {
-    const post = await new Post({
-      title: req.body.title,
-      content: req.body.content,
-      date: req.body.date,
-      author: req.body.author,
-    }).save((err) => {
-      if (err) {
-        return next(err);
-      }
-    });
-    Post.find({ content: req.body.content }, (err, result) => {
-      res.send(result);
-    });
-  },
-];
+    function (req, res, next) {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) next(errors)
+        const post = new Post({
+            title: req.body.title,
+            content: req.body.content,
+            date: req.body.date,
+            author: req.body.author,
+            imgURL: req.body.imgURL,
+        }).save((err) => {
+            if (err) {
+                next(err)
+            } else {
+                Post.find({ content: req.body.content }, (err, result) => {
+                    if (err) next(err)
+                    res.send(result)
+                })
+            }
+        })
+    },
+]
 exports.get_post = function (req, res, next) {
-  Post.find({ _id: req.params.id }, (err, result) => {
-    res.send(result);
-  });
-};
+    Post.find({ _id: req.params.id }, (err, result) => {
+        res.send(result)
+    })
+}
 exports.delete_post = function (req, res, next) {
-  Post.deleteOne({ _id: req.params.id }, (err, docs) => {
-    res.send(docs);
-  });
-};
+    Post.deleteOne({ _id: req.params.id }, (err, docs) => {
+        res.send(docs)
+    })
+}
 
-exports.update_post = function (req, res, next) {
-  Post.updateOne(
-    { _id: req.params.id },
-    { content: req.body.content },
-    (err, docs) => {
-      res.send(docs);
-    }
-  );
-};
+exports.update_post = [
+    body('content')
+        .trim()
+        .isLength({ min: 1 })
+        .escape()
+        .withMessage('Content must be specified.'),
+    function (req, res, next) {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) next(errors)
+        Post.updateOne(
+            { _id: req.params.id },
+            { content: req.body.content },
+            (err, docs) => {
+                if (err) next(err)
+                res.send(docs)
+            }
+        )
+    },
+]
